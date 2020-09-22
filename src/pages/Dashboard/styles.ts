1a942c00c80b66c9e1243ad425aad69ac271a013
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-22 12:41:20 -0300

    Error handler

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -9,7 +13,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -18,9 +22,16 @@ export const Form = styled.form`
     flex: 1;
     height: 70px;
     padding: 0 24px;
-    border: 0;
+    border: 2px solid #fff;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `};
 
     &::placeholder {
       color: #a8a8b3;
@@ -41,6 +52,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 30px;
 `;
 
 export const Repositories = styled.div`
@@ -69,13 +86,13 @@ export const Repositories = styled.div`
   }
 
   img {
-    width: 64px;
-    height: 64px;
+    width: 60px;
+    height: 60px;
     border-radius: 50%;
   }
 
   div {
-    margin-left: 0 16px;
+    margin-left: 16px;
     flex: 1;
 
     strong {
