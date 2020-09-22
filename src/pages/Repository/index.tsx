a0b283df59317bdc97253dd4bf9e555c49bdddaa
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2020-09-22 19:07:27 -0300

    Repositories Page style

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import LogoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +11,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={LogoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>title</strong>
+            <p>descri</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/dashboard">
+          <div>
+            <strong>asdasd</strong>
+            <p>asdasdas</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
